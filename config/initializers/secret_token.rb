0a1ebfaf8e902bb733f4008b1e7731139097f749
unknown <Monomachus@.(none)>
2010-12-19 09:12:58 +0200

    Configured rspec and spork to work with

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArithmeticsTest::Application.config.secret_token = '57552b3d0f23fc38d1dfe6ae215ce012f5b870e41aba6010b95cca76b33205ba595a6ba8e4f9f181e393d9dc4cd9d516783dc49560be706c57c6690bf689fd1f'
